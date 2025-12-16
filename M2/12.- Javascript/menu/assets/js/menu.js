da8c484c7f49f4c3022b0d5790f05138598d1160
Ricardo Vega <rvega@dataqu.cl>
2025-12-16 19:55:22 -0300

    Ejemplo de menú con javascript

---
+++ M2/12.- Javascript/menu/assets/js/menu.js
@@ -0,0 +1,32 @@
+let opcion;
+
+// for(inicio; condición; incremento){
+//     //ejecución del código
+// }
+
+do{
+    //1° Capturar lo ingrese el usuario
+    opcion = prompt(
+        "MEMÚ PRINCIPAL\n" +
+        "1. Saludar\n" +
+        "2. Mostrar fecha\n" +
+        "3. Salir\n\n" +
+        "Elige una opción:"
+    )
+
+    switch(opcion){
+        case "1":
+            alert("Hola! Bienvenido al programa");
+            break;
+        case "2":
+            //let fecha = new Date();
+            alert("Fecha actual: 16-12-2025");
+            break;
+        case "3":
+            alert("Saliendo del programa...");
+            break;
+        default:
+            alert("Opción no válida, intentelo nuevamente");
+    }
+    //ejecución del código
+}while(opcion !== "3");
