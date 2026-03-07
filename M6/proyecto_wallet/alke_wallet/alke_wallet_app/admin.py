c699de1c01c47ee8c158fb2450da942ea7b82506
Ricardo Vega <rvega@dataqu.cl>
2026-03-07 00:40:22 -0300

    Incorporación de modelo compras

--- M6/proyecto_wallet/alke_wallet/alke_wallet_app/admin.py
+++ M6/proyecto_wallet/alke_wallet/alke_wallet_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Compra
 
-# Register your models here.
+#1° Forma, simple
+#admin.site.register(Compra)
+
+#2° Forma más pro
+@admin.register(Compra)
+class CompraAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "detalle", "total", "created_at")
+    list_filter = ("created_at",)
+    search_fields = ("user__username", "user__email")
+    ordering = ("created_at",)
--- M6/proyecto_wallet/alke_wallet/alke_wallet_app/models.py
+++ M6/proyecto_wallet/alke_wallet/alke_wallet_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Compra(models.Model):
+    #el id se crea de manera automática
+    user        = models.ForeignKey(User, on_delete=models.CASCADE, related_name="compras")
+    detalle     = models.CharField(max_length=255)
+    total       = models.IntegerField()
+    created_at  = models.DateTimeField(auto_now_add=True) 
+
+    class Meta:
+        #Permiso personalizado (opcional, aparecerá en el admin)
+        permissions = [
+            ("ver_lista_compras", "Puede ver el listado de compras realizadas")
+        ]
+
+    def __str__(self):
+        return f"Compra #{self.id} - {self.user.username} - ${self.total}"
+
+
+class Transferencia(models.Model):
+    usuario_origen      = models.ForeignKey(User, on_delete=models.CASCADE, related_name="transferencias")
+    destinatario        = models.ForeignKey(User, on_delete=models.CASCADE, related_name="transferencias")
+    created_at          = models.DateTimeField(auto_now_add=True) 
+
+
