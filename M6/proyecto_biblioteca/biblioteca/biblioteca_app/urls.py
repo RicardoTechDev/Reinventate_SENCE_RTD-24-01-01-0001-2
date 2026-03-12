44bafb11b7787abdd27f6d99bb2d68cd73c09914
Ricardo Vega <rvega@dataqu.cl>
2026-03-12 20:52:37 -0300

    Libros

--- M6/proyecto_biblioteca/biblioteca/biblioteca_app/urls.py
+++ M6/proyecto_biblioteca/biblioteca/biblioteca_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import authors_list, publishers_list
+from .views import authors_list, publishers_list, book_list
 
 urlpatterns = [
     #Autores
@@ -9,4 +9,5 @@ urlpatterns = [
     path("publishres/", publishers_list, name="publishers_list"),
 
     #Libros
+    path("books/", book_list, name="book_list"),
 ]
--- M6/proyecto_biblioteca/biblioteca/biblioteca_app/views.py
+++ M6/proyecto_biblioteca/biblioteca/biblioteca_app/views.py
@@ -6,7 +6,7 @@ from django.db.models import Count
 # Author
 #----------------------
 def authors_list(request):
-    autores = Author.objects.all()#SELECT * FROM Authors
+    autores = Author.objects.all().order_by("name")#SELECT * FROM Authors
 
     contexto = {
         "autores" : autores,
@@ -26,3 +26,64 @@ def publishers_list(request):
     } 
 
     return render(request, "publishers_list.html", contexto)
+
+
+#----------------------
+# Book
+#----------------------
+def book_list(request):
+    '''
+    Recuperar información de la base de datos
+    Ejemplos:
+
+    1.- libros = Book.objects.all()
+    2.- libros = Book.objects.value('id', 'name')
+    3.- libros = Book.objects.get(name="nombre_libro") --> puede ser cualquier campo como id u otro
+    4.- libros = Book.objects.filter(title__icontains="Harry") --> Libros que el título contenga "Harry"
+    5.- libros = Book.objects.filter(title__startswith="hola") --> libros que el título comience con "hola"
+    6.- Limitación QuerySet:  
+                            - Book.objects.all()[:5]  --> esto devuelve los primeros 5 objetos
+                            - Book.objects.all()[5:10] --> esto devuelve los objetos del sexto al décimo
+
+    Ejemplo para insertar un registro
+
+    1° Paso recuperar el autor (una instancia) --> this_author = Author.objects.get(id=2)
+    2° Paso creamos un nuevo registro my_book = Book.objects.create(title="Little Women", author=this_author)	# establecer el autor recuperado como el autor de un nuevo libro
+
+    # o en una línea...
+    my_book = Book.objects.create(title="Little Women", author=Author.objects.get(id=2)
+
+    Ejemplo de actualización
+    
+    libro_recuperado = Book.objects.get(id=1)
+    libro_recuperado.title = "Nuevo título"
+    libro_recuperado.save()
+
+    Ejmplo de eliminación
+    1° Foma larga
+    libro_recuperado = Book.objects.get(id=1)
+    libro_recuperado.delete()
+
+    2° Forma corta
+    Book.objects.get(id=1).delete()
+
+    '''
+
+    '''
+    select_related
+    (https://docs.djangoproject.com/en/6.0/ref/models/querysets/#django.db.models.query.QuerySet.select_related)
+
+    Devuelve un QuerySet que "seguirá" las relaciones de clave externa (clave foranea), 
+    seleccionando datos adicionales de objetos relacionados al ejecutar su consulta. 
+    Esto mejora el rendimiento, ya que genera una consulta más compleja, 
+    pero evita que el uso posterior de relaciones de clave externa requiera 
+    consultas a la base de datos.
+    '''
+
+    libros = Book.objects.select_related("author").order_by("title")
+
+    contexto = {
+        "libros" : libros,
+    } 
+
+    return render(request, "books_list.html", contexto)
